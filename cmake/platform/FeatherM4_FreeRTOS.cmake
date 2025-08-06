0b0ff573a3f7e5b4f665e089281770c4bd6b072d
laboratory10 <laboratory10@users.noreply.github.com>
2025-08-06 15:47:59 -0700

    Updates for fprime devel v4

--- cmake/platform/FeatherM4_FreeRTOS.cmake
+++ cmake/platform/FeatherM4_FreeRTOS.cmake
@@ -6,11 +6,10 @@
 if(NOT DEFINED ARDUINO_FQBN)
     message(FATAL_ERROR "Must defined arduino FQBN")
 elseif(CMAKE_SYSTEM_PROCESSOR STREQUAL "arm")
-    set(ARDUINO_TYPES_DIR "${FPRIME_PROJECT_ROOT}/lib/fprime-featherm4-freertos/fprime-arduino/cmake/platform/types/arm") 
+    set(ARDUINO_TYPES_DIR "${FPRIME_PROJECT_ROOT}/lib/fprime-featherm4-freertos/cmake/platform/arm/Platform") 
 else()
-    set(ARDUINO_TYPES_DIR "${FPRIME_PROJECT_ROOT}/lib/fprime-featherm4-freertos/fprime-arduino/cmake/platform/types/basic")
+    set(ARDUINO_TYPES_DIR "${FPRIME_PROJECT_ROOT}/lib/fprime-featherm4-freertos/cmake/platform/basic/Platform")
 endif()
-set(ARDUINO_PLATFORM_DIR "${FPRIME_PROJECT_ROOT}/lib/fprime-featherm4-freertos/fprime-arduino/cmake/platform")
 
 set(CMAKE_EXECUTABLE_SUFFIX "${FPRIME_ARDUINO_EXECUTABLE_SUFFIX}" CACHE INTERNAL "" FORCE)
 
@@ -19,20 +18,6 @@ set(CMAKE_EXECUTABLE_SUFFIX "${FPRIME_ARDUINO_EXECUTABLE_SUFFIX}" CACHE INTERNAL
 # of the fprime-arduino utilities
 set(FPRIME_PLATFORM "ArduinoFw")
 
-# Add FPrime OSAL Implementations
-choose_fprime_implementation(Os/File Os_File_Stub)
-choose_fprime_implementation(Os/Queue Os_Generic_PriorityQueue)
-choose_fprime_implementation(Os/Cpu Os/Cpu/Stub)
-choose_fprime_implementation(Os/Memory Os/Memory/Stub)
-
-# Add fprime-freertos OSAL Implementations
-choose_fprime_implementation(Os/Task Os_Task_FreeRTOS)
-choose_fprime_implementation(Os/Mutex Os_Mutex_FreeRTOS)
-choose_fprime_implementation(Os/ConditionVariable Os_ConditionVariable_FreeRTOS)
-
-
-# Add Arduino OSAL Implementations
-choose_fprime_implementation(Os/Console Os_Console_Arduino)
-choose_fprime_implementation(Os/RawTime Os_RawTime_Arduino)
-
-include_directories("${ARDUINO_TYPES_DIR}" "${ARDUINO_PLATFORM_DIR}")
+include_directories("${CMAKE_CURRENT_LIST_DIR}")
+include_directories("${FPRIME_PROJECT_ROOT}/lib/fprime-featherm4-freertos/fprime-arduino/Arduino")
+add_fprime_subdirectory("${ARDUINO_TYPES_DIR}")

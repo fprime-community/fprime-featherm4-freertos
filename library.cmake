7ceb0bea68232d379e932a90f5d501662b640a3b
laboratory10 <laboratory10@users.noreply.github.com>
2026-03-26 12:32:48 -0700

    Making config include changes based on reference project PR (#7)

--- library.cmake
+++ library.cmake
@@ -3,15 +3,19 @@
 add_fprime_subdirectory("${CMAKE_CURRENT_LIST_DIR}/FeatherM4FreeRTOS/Svc/FeatherM4FatalHandler")
 add_fprime_subdirectory("${CMAKE_CURRENT_LIST_DIR}/FeatherM4FreeRTOS/Svc/OsResources")
 
-# Force inclusion of project-specific FreeRTOSconfig instead of the one in the submodule
-add_compile_options(-include "${CMAKE_SOURCE_DIR}/config/FreeRTOSConfig/FreeRTOSConfig.h")
+# Force inclusion of project-specific or platform-default FreeRTOSconfig instead of the one in the FreeRTOS-SAMD51 submodule
+if(EXISTS "${CMAKE_SOURCE_DIR}/FreeRTOSConfig/FreeRTOSConfig.h")
+ add_compile_options(-include "${CMAKE_SOURCE_DIR}/FreeRTOSConfig/FreeRTOSConfig.h")
+else()
+ add_compile_options(-include "${CMAKE_CURRENT_LIST_DIR}/FeatherM4FreeRTOS/FreeRTOSConfig/FreeRTOSConfig.h")
+endif()
 
 # Set the path to the libraries
 set(FREERTOS_LIB_PATH "${CMAKE_SOURCE_DIR}/lib/FreeRTOS-SAMD51/src")
 set(ARDUINO_LIB_PATH "$ENV{HOME}/Arduino/libraries")
 set(ARDUINO_SAMD_LIB_PATH "$ENV{HOME}/.arduino15/packages/adafruit/hardware/samd/1.7.16/libraries")
 
-# Include directories for Arduino libraries
+# Include directories for FreeRTOS and Arduino libraries
 include_directories(
     ${FREERTOS_LIB_PATH}
     ${ARDUINO_LIB_PATH}/Time
@@ -29,5 +33,4 @@ file(GLOB FREERTOS_SOURCES
 register_fprime_library(
     FreeRTOS
     SOURCES ${FREERTOS_SOURCES}
-    HEADERS ${FREERTOS_LIB_PATH}
 )
